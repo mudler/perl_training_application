f39648534369fcb1123394d76ea9ec88ca074331
Nick Singer <nsinger@suse.de>
2017-10-24 15:34:14 +0200

    Add random ID, add input, catch wrong IDs (#2)
    * Add random id if it is not set on request
    
    * Add template for new pastes

--- lib/PasteBin/Controller/Paste.pm
+++ lib/PasteBin/Controller/Paste.pm
@@ -7,16 +7,21 @@ has 'DB' => sub { PasteBin::DB::Storable->new()->bucket('paste') };
 # This action will render a template
 sub create {
   my $self = shift;
-  my $paste = $self->param('id');
+  my @chars = ("A".."Z", "a".."z", 1..9);
+  my $id = $self->param('id');
   my $content = $self->param('content');
-  $self->DB->set($paste,$content);
-  $self->render(text => $self->DB->get($paste) );
+
+  $id = 0 + $id <= 0 ? (join '' => map $chars[rand @chars], 1 .. 8) : $id;
+
+  $self->DB->set($id,$content);
+  $self->redirect_to("show/$id");
 }
 
 sub show {
   my $self = shift;
   my $paste = $self->param('id');
-  $self->render('show.pm', text => $self->DB->get($paste) );
+  my $data = $self->DB->get($paste);
+  $self->render('show.pm', text => $data ? $data : "<b>Sorry… Paste called $paste not found!</b>");
 }
 
 !!42;
